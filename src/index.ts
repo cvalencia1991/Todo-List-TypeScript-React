1ad8a00d56887fcfdd1491aa0330e6cca8935d84
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-04 21:29:41 -0500

    Fix linters Errors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import * as http from 'http';
+import * as http from "http";
 
 const server = http.createServer((req, res) => {
 
-    if(req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write('Hello World');
+    if(req.url === "/") {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Hello World");
         res.end();
     } else {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write('{"message": "Hello Json!"}');
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.write("{\"message\": \"Hello Json!\"}");
         res.end();
     }
 });
