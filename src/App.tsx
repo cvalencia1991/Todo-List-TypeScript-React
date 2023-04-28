c4d7a1f00a2769d45be1e8d4b1c9b2ebcce98f4f
cvalencia1991 <cesar4a6z@gmail.com>
2023-04-28 16:19:04 -0500

    Understand Values of the elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 
+import React, { useState } from "react";
 import "./App.css";
+import { type } from "os";
 
-function App(): JSX.Element {
-  return (
-    <h1>hello world</h1>
-  );
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface ITask {
+  name:string,
+  done:boolean
 }
+
+
+const App = (): JSX.Element => {
+
+const[newTask, setsnewTask] = useState<string>("");
+const [tasks, setTasks] = useState<ITask[]>([]);
+
+  const handleSubmit = (e:FormElement) =>{
+    e.preventDefault();
+    addTasks(newTask);
+    console.log(tasks);
+  };
+
+  const addTasks =  (name:string) =>{
+    const newtasks = [...tasks,{name,done:false}];
+    setTasks(newtasks);
+  };
+
+  return (
+    <>
+      <form onSubmit ={handleSubmit}>
+        <input type="text"  onChange={e => setsnewTask(e.target.value)} value={newTask}/>
+        <button>Save</button>
+      </form>
+      {
+        tasks.map((t:ITask, index:number)=>{
+          return <h1 key={index}> {t.name}</h1>;
+        })
+      }
+    </>
+  );
+};
 
 export default App;
