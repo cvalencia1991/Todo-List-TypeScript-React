c84daf9fd8f81d59cea15fdf4e814a3dd1c7b316
Cesar Valencia <cesar4a6z@gmail.com>
2023-12-19 21:19:09 -0500

    Update Design white space buttom

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import TodoForm from "./components/TodoForm";
 
 const App = (): JSX.Element => {
   return (
-    <main>
+    <main className="flex flex-col absolute h-full justify-between">
       <TodoForm/>
+      <div className="w-full  h-24 bg-none desktop:bg-white "></div>
     </main>
   );
 };
